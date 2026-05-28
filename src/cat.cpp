c64e20019c77876a1ff85747a90622d5ae7f0106
lucas-watkins <lucas-watkins@outlook.com>
2026-05-28 00:57:15 -0400

    log statement to log http error

--- src/cat.cpp
+++ src/cat.cpp
@@ -1,5 +1,6 @@
 #include <cat.hpp>
 #include <util.hpp>
+#include <logging.hpp>
 
 dpp::slashcommand juno::cat::make_command(dpp::cluster& cluster) const {
     return dpp::slashcommand{ name(), "Cats as a Service!", cluster.me.id }
@@ -29,6 +30,7 @@ void juno::cat::on_command_execution(const dpp::slashcommand_t& event, dpp::clus
 
 void juno::cat::post_image_to_channel(const dpp::slashcommand_t& event, const dpp::http_request_completion_t& req) {
     if (req.error != dpp::h_success || req.status != 200) {
+        juno::log << logging::loglevel::error << " HTTP Error, HTTP Status -> " << req.status << '\n';
         event.reply(util::error_embed("HTTP Error", std::format("HTTP Status -> {}", req.status)));
         return;
     }
